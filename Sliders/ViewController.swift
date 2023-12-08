84fe6e62577f1b1f116c8877940043686fa359f0
Sergey Balashov <waisted.deals.07@icloud.com>
2023-12-07 16:55:15 -0800

    Fix review comments

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -4,66 +4,66 @@
 //
 //  Created by Sergey Balashow on 11/18/23.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
-    // Outlet for custom view
+    // MARK: - IB Outlets
+    @IBOutlet weak var colorView: UIView!
     
-    @IBOutlet weak var myView: UIView!
-    
-    // Outlets for labels
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
-    
-    // Outlets for sliders
+   
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
-    
-    
+    // MARK: - Origin Settings
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        myView.layer.cornerRadius = 20
+        colorView.layer.cornerRadius = 20
+        updateColor()
+        
+        redLabel.text = string(from: redSlider)
+        greenLabel.text = string(from: greenSlider)
+        blueLabel.text = string(from: blueSlider)
+    }
+    
+    // MARK: - Update labels.
+    @IBAction func sliderAction(_ sender: UISlider) {
+        
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenLabel.text = string(from: greenSlider)
+        default:
+            blueLabel.text = string(from: blueSlider)
+        }
+        
+        // Update color for view.
         updateColor()
     }
     
-    // IBActions for Sliders
-    
-    @IBAction func redSliderAction() {
-        updateColor()
-        updateLabels()
+    // Convert slider value to String.
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
     
-    @IBAction func greenSliderAction() {
-        updateColor()
-        updateLabels()
-    }
-    
-    @IBAction func blueSliderAction() {
-        updateColor()
-        updateLabels()
-    }
-    
-    // Update color for custom view
-    func updateColor() {
+    // MARK: - Update color for view.
+    private func updateColor() {
         let red = CGFloat(redSlider.value)
         let green = CGFloat(greenSlider.value)
         let blue = CGFloat(blueSlider.value)
         
-        let color = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
-        myView.backgroundColor = color
+        colorView.backgroundColor = UIColor(
+            red: red,
+            green: green,
+            blue: blue,
+            alpha: 1.0
+        )
     }
-    
-    // Update label values
-    func updateLabels() {
-            redLabel.text = String(format: "%.2f", redSlider.value)
-            greenLabel.text = String(format: "%.2f", greenSlider.value)
-            blueLabel.text = String(format: "%.2f", blueSlider.value)
-        }
 }
 
